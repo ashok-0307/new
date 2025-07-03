c1a2696fefab1329a1cfd30eca2b92a9798d16ad
Ashok Kumar <ashokkumar_a@outlook.com>
2025-07-03 14:02:57 +0530

    commit all

---
+++ helloworld.java
@@ -0,0 +1,9 @@
+
+class Hello {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+        for(int i=1; i<=10; i++){
+            System.out.println("Hello world");
+        }
+    }
+}
